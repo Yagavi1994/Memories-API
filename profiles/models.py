74c0a29c95d920e4a65c2235ba0003a77e1d1418
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-11-06 11:34:20 +0000

    Edit the image size in posts and profiles models.

--- profiles/models.py
+++ profiles/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-
+from PIL import Image  # Import Pillow for image processing
 
 class Profile(models.Model):
+    """
+    Profile model for user profiles.
+    """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='images/Profile-pic_dxmgt2'
-    )
+    image = models.ImageField(upload_to='profile_images/', default='profile_images/default-profile.jpg')
 
     class Meta:
         ordering = ['-created_at']
@@ -19,10 +19,23 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.owner}'s profile"
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image:
+            img = Image.open(self.image.path)
+
+            # Make the image square by cropping or resizing
+            min_dimension = min(img.size)
+            left = (img.width - min_dimension) / 2
+            top = (img.height - min_dimension) / 2
+            right = (img.width + min_dimension) / 2
+            bottom = (img.height + min_dimension) / 2
+
+            img = img.crop((left, top, right, bottom))
+            img = img.resize((min_dimension, min_dimension), Image.ANTIALIAS)
+            img.save(self.image.path)
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
-
-
-post_save.connect(create_profile, sender=User)
