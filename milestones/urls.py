50312ca11e64dc1d471c4562787d6d0edb46ae5d
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-11-12 13:40:03 +0000

    Change the urls of milestone update form update to edit.

--- milestones/urls.py
+++ milestones/urls.py
@@ -5,5 +5,5 @@ from milestones import views
 urlpatterns = [
     path('milestones/', views.MilestoneList.as_view()),
     path('milestones/<int:pk>/', views.MilestoneDetail.as_view()),
-    path('milestones/<int:id>/update/', views.MilestoneUpdate.as_view(), name='milestone-update'),
+    path('milestones/<int:id>/edit/', views.MilestoneUpdate.as_view(), name='milestone-update'),
 ]
