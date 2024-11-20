fbae29d137bcd97eac59e9eddffa09819e902034
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-11-20 17:09:53 +0000

    Delete the signals.py from profiles app.

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class YourAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
-
-    def ready(self):
-        import profiles.signals  # Import the signals module here
